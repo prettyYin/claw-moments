b28b9a1c7ac605462f96ef2287df6c46b17c6448
yzj <2544212327@qq.com>
2024-05-03 23:29:49 +0800

    fix:mapper.xml的表名更改为claw_前缀

--- claw-admin/claw-admin-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
+++ claw-admin/claw-admin-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
@@ -222,7 +222,8 @@ public class SysUserController extends BaseController
         SysUser user = userService.selectUserById(userId);
         List<SysRole> roles = roleService.selectRolesByUserId(userId);
         ajax.put("user", user);
-        ajax.put("roles", SysUser.isAdmin(userId) ? roles : roles.stream().filter(r -> !r.isAdmin()).collect(Collectors.toList()));
+//        ajax.put("roles", SysUser.isAdmin(userId) ? roles : roles.stream().filter(r -> !r.isAdmin()).collect(Collectors.toList()));
+        ajax.put("roles", roles);
         return ajax;
     }
 
