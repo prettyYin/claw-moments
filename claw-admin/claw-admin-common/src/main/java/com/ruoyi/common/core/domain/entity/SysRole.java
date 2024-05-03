aabf52f2390d34791004e7d125c876a35c3ee118
yzj <2544212327@qq.com>
2024-05-03 23:45:26 +0800

    fix:修改表名、状态注释

--- claw-admin/claw-admin-common/src/main/java/com/ruoyi/common/core/domain/entity/SysRole.java
+++ claw-admin/claw-admin-common/src/main/java/com/ruoyi/common/core/domain/entity/SysRole.java
@@ -48,11 +48,11 @@ public class SysRole extends BaseEntity
     /** 部门树选择项是否关联显示（0：父子不互相关联显示 1：父子互相关联显示 ） */
     private boolean deptCheckStrictly;
 
-    /** 角色状态（0正常 1停用） */
-    @Excel(name = "角色状态", readConverterExp = "0=正常,1=停用")
+    /** 角色状态（1正常 0停用） */
+    @Excel(name = "角色状态", readConverterExp = "1=正常,0=停用")
     private String status;
 
-    /** 删除标志（0代表存在 2代表删除） */
+    /** 删除标志（0代表存在 -1代表删除） */
     private String delFlag;
 
     /** 用户是否存在此角色标识 默认不存在 */
